b7e64b68331411f0f2e3a8c045134a949e774748
Junwei Zhao <wahyd4@gmail.com>
2020-02-29 10:41:01 +1100

    Init project with build view entities and models

---
+++ view/entities.go
@@ -0,0 +1,57 @@
+package view
+
+// Organisation represents a Organisation view entity
+type Organisation struct {
+	ID            int      `json:"_id"`
+	URL           string   `json:"url"`
+	ExternalID    string   `json:"external_id"`
+	Name          string   `json:"name"`
+	DomainNames   []string `json:"domain_names"`
+	CreatedAt     string   `json:"created_at"`
+	Details       string   `json:"details"`
+	SharedTickets bool     `json:"shared_tickets"`
+	Tags          []string `json:"tags"`
+}
+
+// User represents a user view entity
+type User struct {
+	ID             int      `json:"_id"`
+	URL            string   `json:"url"`
+	ExternalID     string   `json:"external_id"`
+	Name           string   `json:"name"`
+	Alias          string   `json:"alias"`
+	CreatedAt      string   `json:"created_at"`
+	Active         bool     `json:"active"`
+	Verified       bool     `json:"verified"`
+	Shared         bool     `json:"shared"`
+	Locale         string   `json:"locale"`
+	Timezone       string   `json:"timezone"`
+	LastLoginAt    string   `json:"last_login_at"`
+	Email          string   `json:"email"`
+	Phone          string   `json:"phone"`
+	Signature      string   `json:"signature"`
+	OrganizationID int      `json:"organization_id"`
+	Tags           []string `json:"tags"`
+	Suspended      bool     `json:"suspended"`
+	Role           string   `json:"role"`
+}
+
+// Ticket represents a ticket view entity
+type Ticket struct {
+	ID             string   `json:"_id"`
+	URL            string   `json:"url"`
+	ExternalID     string   `json:"external_id"`
+	CreatedAt      string   `json:"created_at"`
+	Type           string   `json:"type"`
+	Subject        string   `json:"subject"`
+	Description    string   `json:"description"`
+	Priority       string   `json:"priority"`
+	Status         string   `json:"status"`
+	SubmitterID    int      `json:"submitter_id"`
+	AssigneeID     int      `json:"assignee_id"`
+	OrganizationID int      `json:"organization_id"`
+	Tags           []string `json:"tags"`
+	HasIncidents   bool     `json:"has_incidents"`
+	DueAt          string   `json:"due_at"`
+	Via            string   `json:"via"`
+}
